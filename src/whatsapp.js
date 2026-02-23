b0690da5520f2dd85110704a2f5ad96d7d0bcf38
Akmal Muhamad Firdaus <akmal.firdaus@spesolution.com>
2026-02-23 17:28:20 +0700

    refactor: change talenta msg

--- src/whatsapp.js
+++ src/whatsapp.js
@@ -5,7 +5,7 @@ export async function sendText(to, text) {
 	try {
 		await axios.post(process.env.NOTIFY_URL, {
 			target: to,
-			text,
+			message: text,
 		});
 	} catch (err) {
 		console.error("Notify Error:", err.response?.data || err.message);
